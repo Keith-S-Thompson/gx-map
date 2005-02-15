b6c3225c64cf2e79d3ecefbfd5ea35ce27fa1b1b
Keith Thompson <kst@sdsc.edu>
2005-02-14 17:58:12 -0800

    [tgcdb/try.pl 1.2]: 'delphi' --> 'delphi.ncsa.uiuc.edu' Added CVS headers Reformatted "new" call

--- tgcdb/try.pl
+++ tgcdb/try.pl
@@ -1,8 +1,15 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -w
+
+# $Id: try.pl,v 1.2 2005-02-14 17:58:12-08 kst Exp $
+# $Source: /home/kst/gx-map-redacted/tgcdb/try.pl,v $
+
 use strict;
 use TGCDB;
 
-my($tg) = new TGCDB (host=>'delphi',db=>'tgcdb_test',user=>'gxmap',passwd=>'gxmap');
+my($tg) = new TGCDB (host   => 'delphi.ncsa.uiuc.edu',
+		     db     => 'tgcdb_test',
+		     user   => 'gxmap',
+		     passwd => 'gxmap');
 $tg->beginTransaction;
 $tg->gxmap_remove_dn('dtf.ncsa.teragrid', 'mshapiro', 'DN/YYY');
 $tg->gxmap_add_dn('dtf.ncsa.teragrid', 'mshapiro', 'DN/XXX');
